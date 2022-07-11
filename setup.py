9994e3f80eaa8da4b7d794d4a9eb151d884aa242
Suzen Fylke <codesue@users.noreply.github.com>
2022-07-11 13:25:23 -0400

    Set tfx upper limit to <2.0.0

--- setup.py
+++ setup.py
@@ -29,14 +29,14 @@ from setuptools import setup
 
 # TODO(b/174880612): reduce dependency resolution search space
 REQUIRED_PACKAGES = [
-    'absl-py>=0.9,<1.1',
+    'absl-py>=0.9,<=1.1',
     'jinja2>=3.1,<3.2',
     'matplotlib>=3.2.0,<4',
     'jsonschema>=3.2.0,<4',
-    'tensorflow-model-analysis>=0.36.0,<0.37.0',
-    'tensorflow-metadata>=1.5.0,<1.6.0',
-    'tfx>=1.5.0,<1.6.0',
-    'ml-metadata>=1.5.0,<1.6.0',
+    'tensorflow-model-analysis>=0.36.0,<0.41.0',
+    'tensorflow-metadata>=1.5.0,<2.0.0',
+    'tfx>=1.5.0,<2.0.0',
+    'ml-metadata>=1.5.0,<2.0.0',
     'dataclasses;python_version<"3.7"',
 ]
 
