5497bfb9f6346c2a4611901688333b3f245d655d
Suzen Fylke <codesue@users.noreply.github.com>
2022-11-11 11:01:20 -0500

    Bump version to 1.3.3

--- model_card_toolkit/version.py
+++ model_card_toolkit/version.py
@@ -16,7 +16,7 @@
 # We follow Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = "1"
 _MINOR_VERSION = "3"
-_PATCH_VERSION = "2"
+_PATCH_VERSION = "3"
 
 # When building releases, we can update this value on the release branch to
 # reflect the current release candidate ('rc0', 'rc1') or, finally, the official
