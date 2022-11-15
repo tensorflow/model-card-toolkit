f3189f7ea8f8117b5e9c3aca2163c6482e99c96e
Hannes Hapke <hannes@digits.com>
2022-11-15 16:20:26 +0000

    recreated component stub

---
+++ model_card_toolkit/tfx/component.py
@@ -0,0 +1,26 @@
+"""
+The ModelCardGenerator stub to issue removal notice.
+"""
+
+class ModelCardGenerator():
+    """Component spec for the ModelCardGenerator."""
+    raise NotImplementedError(
+        """
+        Component with all dependencies was move to the TFX Addons project (https://github.com/tensorflow/tfx-addons).
+        The component and its dependencies are maintained by the TFX SIG project.
+
+        Install the tfx addons package with
+
+        ```
+        $ pip install tfx-addons[model_card_generator]
+        ```
+
+        You can then import the component via
+        ```
+        from tfx_addons.model_card_generator.component import ModelCardGenerator
+        ...
+        ```
+
+        and use it as usual.
+        """
+    )
